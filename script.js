74a8d35146794c28668d947bd22309917e409d3b
Henri Caesar B <115932176+Henricaesar@users.noreply.github.com>
2023-08-05 03:21:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings : ["Warehouse Staff", "Warehouse Control", "Forklift Operational", "Inventory System Control"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
